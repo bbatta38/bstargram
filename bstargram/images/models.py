a64f8254f3c617cf6b0f8423240dfff9e2eeb8e6
ByungSoo Kim <bbatta38@gmail.com>
2019-02-27 23:39:12 +1300

    create Like models & add fields on Comment models and Image models

--- bstargram/images/models.py
+++ bstargram/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from bstargram.users import models as user_models
 
 # Create your models here.
 class TimeStampedModel(models.Model):
@@ -12,11 +13,23 @@ class TimeStampedModel(models.Model):
 
 class Image(TimeStampedModel):
 
+  """ Image Models"""
   file = models.ImageField()
   location = models.CharField(max_length=140)
   caption = models.TextField()
+  creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
 
 
 class Comment(TimeStampedModel):
 
+  """ Comment Models """
   message = models.TextField()
+  creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+  image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
+
+
+class Like(TimeStampedModel):
+
+  """ Like Models """
+  creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+  image = models.ForeignKey(Image, on_delete=models.CASCADE, null=True)
