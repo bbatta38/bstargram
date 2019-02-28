ab9b40d184d3c7d4d54dfa458c268c07762b248c
ByungSoo Kim <bbatta38@gmail.com>
2019-03-01 00:04:47 +1300

    create urls of images

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,10 @@ urlpatterns = [
         "users/",
         include("bstargram.users.urls", namespace="users"),
     ),
+    path(
+        "images/",
+        include("bstargram.images.urls", namespace="images"),
+    ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(
