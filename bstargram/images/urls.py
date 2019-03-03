5518f72a4afbcd97d8199429d5676bc5a8ff1622
ByungSoo Kim <bbatta38@gmail.com>
2019-03-03 23:21:17 +1300

    create URL for like

--- bstargram/images/urls.py
+++ bstargram/images/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
         regex=r'^$',
         view=views.Feed.as_view(),
         name='Feed'
+    ),
+    url(
+        regex=r'(?P<image_id>[0-9]+)/like/',
+        view=views.LikeImage.as_view(),
+        name="LikeImage"
     )
 ]
