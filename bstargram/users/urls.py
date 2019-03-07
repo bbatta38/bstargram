3fc2141b91ed706c54aac5390eb8b22c0043fd8a
ByungSoo Kim <bbatta38@gmail.com>
2019-03-07 22:42:07 +1300

    add facebook registration

--- bstargram/users/urls.py
+++ bstargram/users/urls.py
@@ -43,4 +43,9 @@ urlpatterns = [
         view=views.ChangePassword.as_view(),
         name="change_password",
     ),
+    url(
+        regex=r'^rest-auth/facebook/$',
+        view=views.FacebookLogin.as_view(),
+        name='fb_login'
+    ),
 ]
