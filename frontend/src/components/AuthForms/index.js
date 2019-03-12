8eca714bbab79ce4dd93e8cea036e780cbc629a7
ByungSoo Kim <bbatta38@gmail.com>
2019-03-12 21:59:55 +1300

    complete auth view

--- frontend/src/components/AuthForms/index.js
+++ frontend/src/components/AuthForms/index.js
@@ -1,32 +1,32 @@
 import React from "react";
 import LogoFacebook from "react-ionicons/lib/LogoFacebook";
+import styles from "./styles.scss";
 
 export const LoginForm = props => (
-    <div>
-        <form>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
-            <input type="submit" value="Log in" />
+    <div className={styles.formComponent}>
+        <form className={styles.form}>
+            <input className={styles.textInput} type="text" placeholder="username" autoComplete="username" />
+            <input className={styles.textInput} type="password" placeholder="password" autoComplete="current-password" />
+            <input className={styles.button} type="submit" value="Log in" />
         </form>
-        <span>or</span>
-        <span><LogoFacebook fontSize="20px" color="#385185" />Log in with Facebook</span>
-        <span>Forgot password?</span>
+        <span className={styles.divider}>or</span>
+        <span className={styles.facebookLink}><LogoFacebook fontSize="20px" color="#385185" />Log in with Facebook</span>
+        <span className={styles.forgotPass}>Forgot password?</span>
     </div>
 );
 
 export const SignupForm = props => (
-    <div>
-        <h3>Sign up to see photos and videos from your friends.</h3>
-        <button><LogoFacebook fontSize="20px" color="white" />log in with Facebook</button>
-        <span>or</span>
-        <form>
-            <input type="email" placeholder="Email" />
-            <input type="text" placeholder="Name" />
-            <input type="username" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <input type="submit" value="Sign up" />
+    <div className={styles.formComponent}>
+        <button className={styles.button}><LogoFacebook fontSize="20px" color="white" />log in with Facebook</button>
+        <span className={styles.divider}>or</span>
+        <form className={styles.form}>
+            <input className={styles.textInput} type="email" placeholder="Email" />
+            <input className={styles.textInput} type="text" placeholder="Name" />
+            <input className={styles.textInput} type="username" placeholder="Username" autoComplete="username" />
+            <input className={styles.textInput} type="password" placeholder="Password" autoComplete="password" />
+            <input className={styles.button} type="submit" value="Sign up" />
         </form>
-        <p>
+        <p className={styles.comment}>
             By signing up, you agree to our <span>Terms & Privacy policy</span>.
         </p>
     </div>
